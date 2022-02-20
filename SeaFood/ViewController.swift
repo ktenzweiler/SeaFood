e700cae78aee1748e1d7ec8ddb2ca70b430461a7
ktenzweiler <ktenzweiler@gmail.com>
2022-02-20 13:13:50 -0600

    first commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Kyle Enzweiler on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
